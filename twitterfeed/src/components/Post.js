ac133a6798e73d198ca306e55def0113387f2bd4
Rojina431 <59170429+Rojina431@users.noreply.github.com>
2021-03-29 20:29:33 +0545

    Update Post.js

--- twitterfeed/src/components/Post.js
+++ twitterfeed/src/components/Post.js
@@ -1,43 +1,62 @@
 import React,{useState,useEffect} from "react";
 import "./Post.css";
 import axios from 'axios';
-import { Avatar } from "@material-ui/core";
+import { Avatar, CircularProgress } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 import { Card } from '@material-ui/core';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
   function Post() {
-
     const [Tweets, setTweets] = useState([]);
- 
+    const [lim,setLim] =useState(7)
     useEffect(() => {
       fetchTweets();
 
-}, [])
+}, [lim])
 
-
+/*window.onscroll=function(){
+  if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+    setLim(prevLim=>prevLim+7);
+  }
+}*/
 const fetchTweets= () => {
-  axios.get('/tweets')
+  axios.get(`/tweets/?count=${lim}`)
   .then(response => {
       if (response.data.success) {
           setTweets(response.data.tweets.statuses)
-          console.log(response.data.tweets.statuses)
       } else {
           alert('Failed to get Tweets')
       }
   })
-  .catch((e)=>{
-    console.log(e.message);
+  .catch((error)=>{
+    console.log(error.message);
   })
 }
+
+
 
 return (
        
     <div>
-      {Tweets[1]&&Tweets.map((tweets, index) => (
+      <InfiniteScroll
+       dataLength={Tweets.length} //This is important field to render the next data
+       next={()=>{
+         setLim(lim+7)
+         console.log(lim)
+        }}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+       hasMore={true}
+       loader={<CircularProgress style={{color:"var(--twitter-color)",padding:"50px"}} disableShrink />}
+       >
+      {Tweets&&Tweets.map((tweets, index) => (
       <React.Fragment key={index}>
         <Card>
         <div className="post" >
@@ -56,21 +75,25 @@ return (
               </h3>
             </div>
             <div className="post__headerDescription">
-             <h1>{tweets.text}</h1>
+             <p>{tweets.text}</p>
             </div>
           </div>
           <img src="" alt="" />
           <div className="post__footer">
-            <ChatBubbleOutlineIcon fontSize="small" />
-            <RepeatIcon fontSize="small" /><span>{tweets.retweet_count}</span>
-      <FavoriteBorderIcon fontSize="small" /><span>{tweets.user.favourites_count}</span>
-            <PublishIcon fontSize="small" />
+          <a className="footer"><ChatBubbleOutlineIcon className="icons"/>
+          <span>{tweets.retweet_count}</span>
+          </a>
+          <a className="footer"><RepeatIcon  className="icons"/><span>{tweets.retweet_count}</span></a>
+          <a className="footer"><FavoriteBorderIcon  className="icons"/><span>{tweets.user.favourites_count}</span>
+          </a>
+          <a className="footer"><PublishIcon  className="icons"/><span>{tweets.retweet_count}</span></a>
           </div>
         </div>
       </div>
       </Card>
     </React.Fragment>
   ))}
+  </InfiniteScroll>
 </div>
     )
 }
